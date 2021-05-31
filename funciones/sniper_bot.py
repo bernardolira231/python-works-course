5db2da276d9454543efd7b32c407db83a5e70372
Bernardo Lira <harritolira@gmail.com>
2021-05-31 08:13:40 -0500

    sniper_bot.py with functions

--- funciones/sniper_bot.py
+++ funciones/sniper_bot.py
@@ -1,19 +1,29 @@
-import requests
+from time import sleep
 from requests_html import HTMLSession
 
 URL = "https://ddtech.mx/producto/tarjeta-de-video-nvidia-geforce-rtx-3070-8gb-evga-xc3-black-gaming-08g-p5-3751-kr-solo-1-pieza-por-cliente?id=8446"
 
 
-def buy_zone(session):
-    buy_zone = session.html.find("#btnsWishAddBuy")
+def get_url(session):
+    url_info = session.get(URL)
+    return url_info
+
+
+def buy_zone(url_info):
+    buy_zone = url_info.html.find(".no-stock")
     return buy_zone
 
 
 def main():
     session = HTMLSession()
-    buy_zone(session)
-    if len(buy_zone) > 0:
-        print("Hay en existencia")
+    get_url(session)
+    while True:
+        if len(buy_zone(get_url(session))) == 0:
+            print("Hay en existencia")
+            break
+        else:
+            print("No hay en existencia")
+        sleep(60)
 
 
 
