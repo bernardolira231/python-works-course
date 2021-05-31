a15432a02650c5b8db6803e663e08af55dedc46e
Bernardo Lira <harritolira@gmail.com>
2021-05-31 10:19:33 -0500

    sniper_bot.py fix open other tab

--- funciones/sniper_bot.py
+++ funciones/sniper_bot.py
@@ -22,15 +22,21 @@ def buy_zone(url_info):
 
 
 def buy_button(driver):
-    driver.find_element_by_class_name("btn btn-primary add-cart").click()
-    driver.find_element_by_class_name("messenger-close").click()
-    driver.find_element_by_class_name("fa fa-shopping-cart").click()
-    sleep(1)
-    driver.find_element_by_id("terms").click()
-    driver.find_element_by_class_name("btn btn-primary cart-checkout-btn").click()
-    sleep(1)
-    driver.find_element_by_id("email_login").click()
-    driver.find_element_by_id("password_login").click()
+    is_loaded = False
+    while not is_loaded:
+        try:
+            driver.find_element_by_class_name("btn btn-primary add-cart").click()
+            sleep(1)
+            driver.find_element_by_class_name("messenger-close").click()
+            driver.find_element_by_class_name("fa fa-shopping-cart").click()
+            sleep(1)
+            driver.find_element_by_id("terms").click()
+            driver.find_element_by_class_name("btn btn-primary cart-checkout-btn").click()
+            sleep(1)
+            driver.find_element_by_id("email_login").click()
+            driver.find_element_by_id("password_login").click()
+        except NoSuchElementException:
+            print("Intentando cargar nuevamente")
 
 
 def main():
@@ -39,8 +45,7 @@ def main():
     while True:
         if len(buy_zone(get_url(session))) == 0:
             print("Hay en existencia")
-            get_url_with_driver().get(URL)
-            buy_button(get_url_with_driver())
+            buy_button(get_url_with_driver().get(URL))
             break
         else:
             print("No hay en existencia")
