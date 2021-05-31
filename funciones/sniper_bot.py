901c09856754bb737575a3321480c97da5a305a2
Bernardo Lira <harritolira@gmail.com>
2021-05-31 09:35:56 -0500

    sniper_bot.py with selenium

--- funciones/sniper_bot.py
+++ funciones/sniper_bot.py
@@ -1,7 +1,13 @@
 from time import sleep
 from requests_html import HTMLSession
+from selenium import webdriver
 
 URL = "https://ddtech.mx/producto/tarjeta-de-video-nvidia-geforce-rtx-3070-8gb-evga-xc3-black-gaming-08g-p5-3751-kr-solo-1-pieza-por-cliente?id=8446"
+
+
+def get_url_with_driver():
+    driver = webdriver.Firefox()
+    return driver
 
 
 def get_url(session):
@@ -14,12 +20,26 @@ def buy_zone(url_info):
     return buy_zone
 
 
+def buy_button(driver):
+    driver.find_element_by_class_name("btn btn-primary add-cart").click()
+    driver.find_element_by_class_name("messenger-close").click()
+    driver.find_element_by_class_name("fa fa-shopping-cart").click()
+    sleep(1)
+    driver.find_element_by_id("terms").click()
+    driver.find_element_by_class_name("btn btn-primary cart-checkout-btn").click()
+    sleep(1)
+    driver.find_element_by_id("email_login").click()
+    driver.find_element_by_id("password_login").click()
+
+
 def main():
     session = HTMLSession()
     get_url(session)
     while True:
         if len(buy_zone(get_url(session))) == 0:
             print("Hay en existencia")
+            get_url_with_driver().get(URL)
+            buy_button(get_url_with_driver())
             break
         else:
             print("No hay en existencia")
