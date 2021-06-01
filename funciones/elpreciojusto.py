3119d5fd080f76939cb4810d387726cb103e9f0b
Bernardo Lira <harritolira@gmail.com>
2021-06-01 10:22:28 -0500

    bug in windows

--- funciones/elpreciojusto.py
+++ funciones/elpreciojusto.py
@@ -10,20 +10,21 @@ def main():
     main_site = session.get("https://www.pccomponentes.com")
     categories = main_site.html.find(".mkt-menu-level3")
 
+
     category = random.choice(categories)
 
-    while category.text  == "Configurador de PCs":
+    while category.text == "Configurador de PCs":
         category = random.choice(categories)
 
     product_page = session.get(category.attrs["href"])
     products = product_page.html.find(".c-product-card__wrapper")
 
     product = random.choice(products)
-    image_src = product.find(".c-product-card__image", first=True).attrs["scrr"]
+    image_src = product.find(".c-product-card__image", first=True).attrs["scr"]
     product_name = product.find(".c-product-card__title", first=True).text
     product_price = product.find(".c-product-card__prices-actual", first=True).txt
 
-    print(product_price)
+    print(float(product_price.replace("€", "").replace(",", ".")))
 
 
 if __name__ == "__main__":
