b231cee1568ea4690426d18084352cb7ef98c3ef
Bernardo Lira <harritolira@gmail.com>
2021-06-02 18:17:01 -0500

    fix bugs

--- funciones/desktop.py
+++ funciones/desktop.py
@@ -2,9 +2,11 @@ import PySimpleGUI as sg
 
 button_size = (7, 3)
 
-layout = [[sg.Button("", size=button_size), sg.Button("", size=button_size), sg.Button("", size=button_size)],
-          [sg.Button("", size=button_size), sg.Button("", size=button_size), sg.Button("", size=button_size)],
-          [sg.Button("", size=button_size), sg.Button("", size=button_size), sg.Button("", size=button_size)],
+layout = [[sg.Button("", key="-1-", size=button_size), sg.Button("", key="-2-", size=button_size), sg.Button("", key="-3-", size=button_size)],
+          [sg.Button("", key="-4-", size=button_size), sg.Button("", key="-5-", size=button_size), sg.Button("", key="-6-", size=button_size)],
+          [sg.Button("", key="-7-", size=button_size), sg.Button("", key="-8-", size=button_size), sg.Button("", key="-9-", size=button_size)],
           [sg.Button("Close")]]
 
-window = sg.Window("Demo", layout).read
+window = sg.Window("Demo", layout)
+value = window.read()
+print(value)
