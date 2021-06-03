06b379d1b56a65c673fd4b1a2eb8a0fe4e4649d5
Bernardo Lira <harritolira@gmail.com>
2021-06-03 13:00:33 -0500

    functional programming start

--- funciones/desktop.py
+++ funciones/desktop.py
@@ -29,24 +29,76 @@ layout = [
              sg.Button("", key="-7-", size=button_size),
              sg.Button("", key="-8-", size=button_size)
          ],
+         [
+             sg.Text("", key="-Winner-")
+         ],
+         [
+             sg.Button("Close", key="-OK-")
+         ]
+         ]
 
-          [sg.Button("Close", key="-OK-")]]
 
-window = sg.Window("Demo", layout)
+def configure_layout():
+    sg.Window("Demo", layout)
 
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == "-OK-":
-        break
 
-    if window.Element(event).ButtonText == "":
-        index = int(event.replace("-", ""))
-        deck[index] = current_player
-        window.Element(event).Update(text = current_player)
+def close_game(event):
+    event == sg.WIN_CLOSED or event == "-OK-"
+    return
 
+
+def create_deck(window, event, game_end):
+    window.Element(event).ButtonText == "" and not game_end
+    return
+
+
+def init_deck(event, window):
+    index = int(event.replace("-", ""))
+    deck[index] = current_player
+    window.Element(event).Update(text = current_player)
+    return
+
+
+def detect_winner_play(game_end):
+    for winner_play in winner_plays:
+        if deck[winner_play[0]] == deck[winner_play[1]] == deck[winner_play[2]] != 0:
+            if deck[winner_play[0]] == PLAYER_ONE:
+                print("El jugador 1 ha ganado!!!")
+            else:
+                print("El jugador 2 ha ganado!!!")
+            game_end = True
+    return game_end
+
+
+def tie(game_end):
+    if 0 not in deck:
+        print("Ha terminado el juego! Nadie ha ganado")
+        game_end = True
+        return game_end
+
+
+def change_current_player():
     if current_player == PLAYER_ONE:
         current_player = PLAYER_TWO
     else:
         current_player = PLAYER_ONE
 
-window.close()
+
+def main():
+    window = configure_layout()
+    game_end = False
+    while True:
+        event, values = window.read()
+        if close_game(event):
+            break
+        if create_deck(window, event, game_end):
+            init_deck(event, window)
+        detect_winner_play(game_end)
+        tie(game_end)
+        change_current_player()
+
+    window.close()
+
+
+if __name__ == "__main__":
+    main()
